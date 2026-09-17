71a5ffc1452a44d4f977ee70035a863146080959
João Paulo <joao.paulo.miquilini@gmail.com>
2026-09-17 14:01:21 -0300

    Add files via upload

---
+++ aula1.py
@@ -0,0 +1,5 @@
+num1 = float(input("Informe o primeiro número da operação: "))
+num2 = float(input("Informe o segundo número da operação: "))
+
+result = num1 + num2
+print("O resultado da soma é:", result)
